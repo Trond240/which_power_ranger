5750f43e38931588caa5b437c4a56211a1cc4383
Alan Turing <trond@blacktie-llc.com>
2019-09-17 17:57:01 -0600

    Added ID's to cards, but broke changing the page.

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 var innerHTML = document.getElementById('#inner-HTML');
 var playerOne = document.getElementById('p-1-input');
 var playerTwo = document.getElementById('p-2-input');
-// var newPlayer1 = document.querySelector();
-// var newPlayer2 = document.querySelector();
 var playGame = document.querySelector('.play-game-button');
 // var button = document.getElementById('play-button');
-// var main = document.querySelector('.play-game-button')
 var mainContainer = document.querySelector('.main-container');
 // var startGameButton = <button class="start-game-button">Start Game</button>;
 // var gamePageButton = <button class="start-game-button">Play Game</button>;
@@ -36,7 +33,7 @@ function gameStartInfo(event) {
       <span>${playerOne.value}</span>AND
       <span>${playerTwo.value}!</span>
    </p>
-   <p>The goal of the game is to find all 5 pairs of the crads as quickly as possible.
+   <p class = 'directions'>The goal of the game is to find all 5 pairs of the crads as quickly as possible.
       the player that finds the greatest numbers of pairs, wins.<br>
       To begin playing, the player whose names is highlighted can click any card. It will flip
       over and reveala picture of a Power Ranger. Click another card. If they match, they will
@@ -60,35 +57,35 @@ function gamePage() {
   console.log('this');
   event.preventDefault();
   var gamePageTemplate = `
-      <article class = 'box-1' id = 'box1'>
-      <h3>${playerOne.value}</h3>
-      <h3>Matches this Round</h3>
-      <p>5</p>
-      <h3>GAME WINS</h3>
-      <p>Round 1</p>
-      <p>Round 2</p>
-      </article>
-      <article class ='box-2' id = 'box2'>
-      <button>card1</buton>
-      <button>card2</buton>
-      <button>card3</buton>
-      <button>card4</buton>
-      <button>card5</buton>
-      <button>card6</buton>
-      <button>card7</buton>
-      <button>card8</buton>
-      <button>card9</buton>
-      <button>card10</buton>
-      </article>
-      <article class ='box-3' id ='box3'>
-      <h3>${playerTwo.value}</h3>
-      <h3>Matches this Round</h3>
-      <p>5</p>
-      <h3>GAME WINS</h3>
-      <p>Round 1</p>
-      <p>Round 2</p>
-      </article>
-    `;
+  <article class = 'box-1' id = 'box1'>
+  <h3>${playerOne.value}</h3>
+  <h3>Matches this Round</h3>
+  <p>5</p>
+  <h3>GAME WINS</h3>
+  <p>Round 1</p>
+  <p>Round 2</p>
+  </article>
+  <article class ='box-2' id = 'box2'>
+  <button id = 'card-one'>card1</buton>
+  <button id = 'card-two'>card2</buton>
+  <button id = 'card-three'>card3</buton>
+  <button id = 'card-four'>card4</buton>
+  <button id = 'card-five'>card5</buton>
+  <button id = 'card-six'>card6</buton>
+  <button id = 'card-seven'>card7</buton>
+  <button id = 'card-eight'>card8</buton>
+  <button id = 'card-nine'>card9</buton>
+  <button id = 'card-ten'>card10</buton>
+  </article>
+  <article class ='box-3' id ='box3'>
+  <h3>${playerTwo.value}</h3>
+  <h3>Matches this Round</h3>
+  <p>5</p>
+  <h3>GAME WINS</h3>
+  <p>Round 1</p>
+  <p>Round 2</p>
+  </article>
+  `;
 
   // var gamePageButton = document.querySelector('.start-game-button').addEventListener('click', gamePage)
 
