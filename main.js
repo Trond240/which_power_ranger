5cd34c27c9ed8017ab75a7dee004f554782ed92e
Trond Makonese <trondation@gmail.com>
2019-11-01 11:38:20 -0600

    added variables for javaScript.

---
+++ main.js
@@ -0,0 +1,11 @@
+var playerOne = document.querySelector('.player1');
+var playerTwo = document.querySelector('.player2');
+var directionsButton = document.querySelector('.game-directions');
+var gamePageButton = document.querySelector('.play-game');
+
+var formSection = document.querySelector('.playerInput');
+var mainContainer = document.querySelector('.main-contianer')
+
+
+formSection.addEventListener('click', formHandler);
+// formSection.addEventListener('click', noPlayer);
